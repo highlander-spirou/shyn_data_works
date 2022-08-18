8f231d728a8f740a036748f53e3f173e8b588c44
highlander-spirou <71387842+highlander-spirou@users.noreply.github.com>
2022-08-18 17:02:35 +0700

    #2 - Đổi tên dataframes - rename df_transformation to datetime_transformation

--- main-package/dataframes/datetime_transform.py
+++ main-package/dataframes/datetime_transform.py
@@ -26,16 +26,6 @@ def get_df_categories_in_year(df, year:int, index:Optional[str]=None):
     returned_df.rename(columns=new_column_list, inplace=True)
     return returned_df
 
-def reduce_inner_join(list_df:Iterable[pd.DataFrame], on:str, index:Optional[str]=None):
-    """
-    # Hàm này dùng reduce để SQL-inner-join nhiều tables
-    - Has the same restrictions as SQL joins
-    """
-    df_merged = reduce(lambda left,right: pd.merge(left,right,on=[on], how='inner'), list_df)
-    if index is not None:
-        df_merged.set_index(index, inplace=True)
-    return df_merged
-
 
 def bin_time_of_day(data, column_to_bin, time_format='%H:%M', column_hour_label='hour'):
     """
