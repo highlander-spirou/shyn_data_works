09e478b39e9f1826754b8cf87e5da1649daf8730
highlander-spirou <71387842+highlander-spirou@users.noreply.github.com>
2023-01-17 14:32:20 +0700

    add draw barh

---
+++ main-package/graphs/barh.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def draw_barh(df, top=None, colors='pastel', y_invert=False):
+    """
+    A wrapper for drawing plt's barh
+    """
+    if top is None:
+        df.plot(kind='barh', stacked=True, color=colors)
+    else:
+        palette = sns.color_palette(colors)
+        df[:top].plot(kind='barh', stacked=True, color=palette)
+    
+    if y_invert: plt.gca().invert_yaxis()
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
