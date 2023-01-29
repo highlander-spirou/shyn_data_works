442638ad1cbea9c398310d00a43240b0c2d67f99
highlander-spirou <71387842+highlander-spirou@users.noreply.github.com>
2023-01-29 20:34:08 +0700

    change geographic_heatmap to accepts default template

--- main-package/graphs/geographic_heatmap.py
+++ main-package/graphs/geographic_heatmap.py
@@ -1,93 +1,133 @@
-import requests
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
+from jinja2 import Environment, FileSystemLoader
+from os import path
+from typing import Literal, TypedDict
 
-from typing import Union, Optional
+class ParamsDict(TypedDict):
+    small_text: str
+    big_text: str
+    render_img: Literal['<img />', '<svg></svg>']
 
-class HighlightProvince:
-    """
-    # Params
-    - geo_source: Nếu là url -> dùng requests serialize thành python dict
-    - map_center: Tuple(lat, long)
-    - id_property: self.geo_data['features'][i]['properties'].keys() - thường là id hoặc cartodb_id
-    - name_property: Là label của province
-    - zoom: map zoom level
-    - hue_column: Hue *MUST* has index as `province` 
-    """
-    def __init__(self, geo_source: Union[dict, str], map_center:tuple, id_property:str, name_property:str = 'name', zoom=2, hue_column:Optional[pd.Series]=None) -> None:
-        self.geo_source = geo_source
-        self.id_property = id_property
-        self.name_property = name_property
-        self.hue_column = hue_column
-        self.map_center = map_center
-        self.zoom = zoom
-        
-    def get_geo_data(self):
-        self.geo_data: dict = dict()
-        if type(self.geo_source) == str:
-            response = requests.get(self.geo_source, timeout=15)
-            json_data: dict = response.json()
-            self.geo_data = json_data
+
+class CardComponent:
+    def __init__(self, template:Literal["default", "templates/file.html", "html str"], params:ParamsDict) -> None:
+        if template == "default":
+            self.template = """
+            <!DOCTYPE html>
+<html lang="en">
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+Gujarati:wght@300;700;900&display=swap");
+
+    boby {
+      width: 450px;
+      height: 150px;
+    }
+    .wrapper {
+      position: relative;
+
+      width: 300px;
+      height: 123px;
+
+      background-color: white;
+
+      border: 0.75px solid rgba(0, 0, 0, 0.1);
+      border-radius: 30px;
+    }
+
+    .circle {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      position: absolute;
+      top: 19%;
+      left: 10%;
+
+      background-color: rgba(0, 0, 0, 0.1);
+      width: 80px;
+      height: 80px;
+
+      border-radius: 50%;
+    }
+
+    .image {
+      margin-top: 3px;
+    }
+
+    .image > img {
+      width: 55px;
+      height: 55px;
+      border-radius: 50%;
+    }
+    .image > svg {
+      width: 55px;
+      height: 55px;
+      background-color: inherit;
+    }
+
+    .box {
+      display: flex;
+      flex-direction: column;
+
+      position: absolute;
+      top: 25%;
+      left: 45%;
+    }
+
+    .small-text {
+      font-family: "Noto Sans Gujarati", sans-serif;
+      font-size: 16px;
+      font-weight: 300;
+      color: rgb(100 116 139);
+    }
+
+    .big-text {
+      margin-top: 10px;
+
+      font-family: "Noto Sans Gujarati", sans-serif;
+      font-size: 32px;
+      font-weight: 700;
+      color: rgb(68, 84, 104);
+    }
+  </style>
+  <body>
+    <div class="wrapper">
+      <div class="circle">
+        <div class="image">
+          {{render_img}}
+        </div>
+      </div>
+      <div class="box">
+        <div class="small-text">{{small_text}}</div>
+        <div class="big-text">{{big_text}}</div>
+      </div>
+    </div>
+  </body>
+</html>
+
+            """
         else:
-            self.geo_data = self.geo_source
-            
-    def create_df(self):
-        province = [self.geo_data['features'][i]['properties'][self.name_property] for i in range(0, len(self.geo_data['features']))]
-        id_ = [self.geo_data['features'][i]['properties'][self.id_property] for i in range(0, len(self.geo_data['features']))]
+            self.template=template
+        self.params = params
         
-        self.df = pd.DataFrame({
-            'province': province,
-            'id_': id_
-        })
+    def init_template(self):
+        if path.exists(self.template):
+            environment = Environment(loader=FileSystemLoader("templates/"))
+            renderer = environment.get_template(self.template.split('/')[1])
+        else:
+            environment = Environment()
+            renderer = environment.from_string(self.template)
+            
+        self.renderer = renderer
+        
+    def render_content(self):
+        self.content = self.renderer.render(**self.params)
+        return self.content
     
-    def add_hue(self):
-        if self.hue_column is not None:
-            self.hue_column.name = 'hue'
-            new_df = self.df.merge(self.hue_column, how='inner', left_on='province', right_index=True)
-            self.df = new_df
-
-        else:
-            self.df['hue'] = 0
-            
-    def setup_plot_params(self):
-        if self.hue_column is not None:
-            customdata = np.stack(self.df['province'], axis=-1)
-            self.params = {
-                'geojson': self.geo_data,
-                'locations': self.df['id_'],
-                'z': self.df['hue'],
-                'featureidkey': f'properties.{self.id_property}',
-                'colorscale': 'Blues',
-                'customdata': customdata,
-                'hovertemplate': '<b>%{customdata}</b>',
-                'hoverinfo': 'z'
-            }
-        else:
-            self.params = {
-                'geojson': self.geo_data,
-                'locations': self.df['id_'],
-                'featureidkey': f'properties.{self.id_property}',
-                'z': self.df['hue'],
-                'colorscale': 'Blues',
-            }
-            
-    def draw_plot(self, save=False, name='file'):
-        fig = go.Figure(go.Choroplethmapbox(**self.params))
-
-
-        fig.update_layout(mapbox_style="white-bg", mapbox_center={'lat': self.map_center[0], 'lon': self.map_center[1]}, mapbox_zoom=self.zoom)
-        fig.update_layout(
-            margin=dict(l=0, r=0, t=0, b=0),
-        )
-        fig.update_traces(marker_line_width=1)
-        fig.show()
-        
-        if save is True:
-            fig.write_html(f"{name}.html")
+    def save_file(self, filename):
+        with open(f"{filename}.html", mode="w", encoding="utf-8") as f:
+            f.write(self.content)
             
     def __call__(self):
-        self.get_geo_data()
-        self.create_df()
-        self.add_hue()
-        self.setup_plot_params()
+        self.init_template()
+        return self.render_content()
+            
