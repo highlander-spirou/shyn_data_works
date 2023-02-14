7965629013ba9309ef861b33155abfe9a751085c
highlander-spirou <71387842+highlander-spirou@users.noreply.github.com>
2023-02-14 16:17:29 +0700

    update version 0.0.2

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 
 setuptools.setup(
     name="shyn_data_works", # pip install
-    version="0.0.1",
+    version="0.0.2",
     packages=setuptools.find_packages(include=['shyn_data_works', 'shyn_data_works.*']), # from shyn_data_works import 
 )
