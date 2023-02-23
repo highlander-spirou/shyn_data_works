ef14668fd7332bee5026aea3d9ae258b5f0459ee
highlander-spirou <71387842+highlander-spirou@users.noreply.github.com>
2023-02-23 16:18:29 +0700

    update dependencies array

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ import setuptools
 
 setuptools.setup(
     name="shyn_data_works", # pip install
-    version="0.0.2",
-    packages=setuptools.find_packages(include=['shyn_data_works', 'shyn_data_works.*']), # from shyn_data_works import 
+    version="0.0.3",
+    packages=setuptools.find_packages(include=['shyn_data_works', 'shyn_data_works.*']), # from shyn_data_works import
+    install_requires=[
+        'prodict',
+        'jupyterlab',
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'plotly',
+        'scipy',
+        'scikit-learn'
+    ] 
 )
